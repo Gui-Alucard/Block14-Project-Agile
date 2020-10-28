eea38fd98e50d6242876b5abdd8e166a7b9ec41d
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-10-28 09:54:41 -0300

    Updates

---
+++ precache-manifest.66630a7683e712d6f970dd06368189ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be2d803293392709d00264090caa90f",
+    "url": "/Block14-Project-Agile/index.html"
+  },
+  {
+    "revision": "f9a1b216215f488672ed",
+    "url": "/Block14-Project-Agile/static/css/main.30fb04a3.chunk.css"
+  },
+  {
+    "revision": "400a2b75aeb53bebbbdf",
+    "url": "/Block14-Project-Agile/static/js/2.88982449.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Block14-Project-Agile/static/js/2.88982449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9a1b216215f488672ed",
+    "url": "/Block14-Project-Agile/static/js/main.3f9a021b.chunk.js"
+  },
+  {
+    "revision": "7c8387eccd4778fd27f1",
+    "url": "/Block14-Project-Agile/static/js/runtime-main.508df4fa.js"
+  }
+]);
